c3d1dbc3dae032af718c69f20602ea1c84d33cd3
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-05-08 02:10:12 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,12 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  puts "Welcome to Tic Tac Toe"
+  counter= 0
+  while counter<9
+    turn(board)
+    counter+=1
+  end
+
+end
